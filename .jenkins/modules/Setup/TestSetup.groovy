f0c17810d6e88d0b8fd1bed0239e5c05e853209f
Cuba Stanley <cuba.stanley@payara.fish>
2022-07-11 13:45:51 +0100

    Perform Base Behaviour

--- .jenkins/modules/Setup/TestSetup.groovy
+++ .jenkins/modules/Setup/TestSetup.groovy
@@ -1,5 +1,8 @@
 def ASADMIN = "${pwd()}/${getPayaraDirectoryName(CFG.'build.version')}/bin/asadmin"
 
+// Perform the base behaviour
+MPLModule('Test Setup', CFG);
+
 echo '*#*#*#*#*#*#*#*#*#*#*#*#  Setting up tests  *#*#*#*#*#*#*#*#*#*#*#*#*#*#*#'
 sh "${ASADMIN} create-domain --nopassword ${CFG.domain_name}"
 sh "${ASADMIN} start-domain ${CFG.domain_name}"
